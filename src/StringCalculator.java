382d6bfb7c8b78f6ba1631779235cc1636b2e09f
aviadfi <Aviad@Aviad-PC>
2013-12-10 19:59:33 +0200

    1 start

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,6 +2,11 @@
 public class StringCalculator {
 
 	public static int add(String text){
+		
 		return 0;
-	}
+		
+	} 
+		
+
+
 }
--- src/StringCalculatorTaster.java
+++ src/StringCalculatorTaster.java
@@ -13,5 +13,17 @@ public class StringCalculatorTaster {
 	
 		
 	}
-
+	
+	@Test
+	
+	public void singleValueReturn(){
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 }
