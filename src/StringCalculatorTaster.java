cd151dda7654047c3be838dfd9c2f3fdee97d0a7
aviadfi <Aviad@Aviad-PC>
2013-12-10 20:45:06 +0200

    2 green

--- src/StringCalculatorTaster.java
+++ src/StringCalculatorTaster.java
@@ -8,9 +8,21 @@ public class StringCalculatorTaster {
   
   @Test
   public void add_Empty_String_Gives_Zero() throws Exception {
+	  
     assertEquals(0, new StringCalculator().add(""));
   
   
   
   }
+  
+  @Test 
+  public void OneNumberReturnsOne() {
+	  
+	    assertEquals(1, new StringCalculator().add("1"));
+	  
+	  
+	  
+	  }
+	  
+  
   }
